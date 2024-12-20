7bd94294289cfcca701d43b9f9ff1cadb843a906
Michal Opielka <michal.opielka@finder.com>
2024-12-21 00:20:17 +0100

    feat: configure build prod

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()]
-})
+export default defineConfig(({ mode }) => ({
+  plugins: [react()],
+  build: {
+    outDir: mode === 'production' ? 'dist-prod' : 'dist', // Custom output for production
+  },
+  base: mode === 'production' ? '/parking' : '',
+}))
